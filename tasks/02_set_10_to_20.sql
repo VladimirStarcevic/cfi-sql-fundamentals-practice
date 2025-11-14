9f6ea5176ae8d5a6b58c3c51e60da967da311e6f
VladimirStarcevic <vladimirstarcevic33@gmail.com>
2025-11-14 23:23:27 +0100

    feat(dql): Implement JOINs with aggregation and advanced filtering
    - Practiced filtering text data using LIKE and NOT LIKE with wildcards.
    - Successfully implemented a complex INNER JOIN between FactInternetSales and DimSalesTerritory to calculate total sales per region.
    - Utilized DataGrip's ER diagram visualization to debug a logically incorrect JOIN condition, reinforcing the importance of using correct primary/foreign key pairs.
    - Generated a visual report (pie chart) from the query result, demonstrating the full data analysis workflow from query to insight.

--- tasks/02_set_10_to_20.sql
+++ tasks/02_set_10_to_20.sql
@@ -23,4 +23,38 @@ ON dc.GeographyKey = dg.GeographyKey
 WHERE dg.City IN('London', 'Paris', 'Berlin')
 GROUP BY dc.FirstName, dc.LastName, dg.City
 
+-- Zadatak 13: LIKE i Wildcards (%, _)
+-- Zadatak: Pronađi sve proizvode (DimProduct) čije ime (EnglishProductName) počinje sa 'Mountain'. Prikaži prvih 15 rezultata.
+
+SELECT TOP 15
+    EnglishProductName AS [Product Name]
+FROM
+    DimProduct
+WHERE EnglishProductName LIKE 'Mountain%';
+
+-- Zadatak 14: NOT LIKE i Kombinacija Uslova
+-- Zadatak: Prikaži sve proizvode (DimProduct) koji ne sadrže reč 'Bike' u svom imenu, a njihova boja je 'Red' ili 'Blue'. Prikaži ime i boju.
+
+SELECT
+    EnglishProductName AS [Product Name],
+    Color
+FROM
+    DimProduct
+WHERE EnglishProductName NOT LIKE '%Bike%' AND Color IN('Red', 'Blue');
+
+-- Zadatak 15: SUM Agregacija
+-- Izračunaj ukupan iznos svih internet prodaja (FactInternetSales) po prodajnoj teritoriji.
+-- Prikaži ime teritorije (SalesTerritoryRegion) i ukupan iznos prodaje. Sortiraj po iznosu opadajuće
+
+SELECT
+    dst.SalesTerritoryRegion AS 'Territory Name',
+    SUM(SalesAmount) AS 'Total Sale'
+FROM
+    FactInternetSales AS FIS
+INNER JOIN
+    DimSalesTerritory AS dst
+ON FIS.SalesTerritoryKey = dst.SalesTerritoryKey
+GROUP BY dst.SalesTerritoryRegion
+ORDER BY SUM(SalesAmount) DESC;
+
 
